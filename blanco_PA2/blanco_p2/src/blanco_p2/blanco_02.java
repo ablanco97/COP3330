2c57457b727e9a26c1fc3541ef2b51f25be9bc45
ablanco97 <49414910+ablanco97@users.noreply.github.com>
2019-09-17 13:28:35 -0400

    second problem done

---
+++ blanco_PA2/blanco_p2/src/blanco_p2/blanco_02.java
@@ -0,0 +1,50 @@
+package blanco_p2;
+
+import java.util.Scanner;
+
+public class blanco_02 {
+
+	public static void main(String[] args) {
+		
+		Scanner scnr = new Scanner(System.in);
+		int weight = 0;
+		double height = 0.0;
+		double BMI = 0.0;
+		
+		System.out.print("BMI CALCULATOR\nSelect 1.Standard or 2.Metric : ");
+		int choice = scnr.nextInt();
+		
+		
+		if(choice == 1) {
+		//standard
+			System.out.print("Enter your weight(pounds): ");
+			weight = scnr.nextInt();
+			System.out.print("Enter your height(inches): ");
+			height = scnr.nextDouble();
+			
+			BMI = (703 * weight)/(height * height);
+			
+			System.out.printf("Your BMI: %.1f\n", BMI);	
+			
+		}else if(choice == 2) { 
+		//metric
+			System.out.print("Enter your weight(kilograms): ");
+			weight = scnr.nextInt();
+			System.out.print("Enter your height(meters): ");
+			height = scnr.nextDouble();
+			
+			BMI = weight / (height * height);
+			
+			System.out.printf("Your BMI: %.1f\n", BMI);
+			
+		}else {
+			System.out.print("Not an option. Enter 1 or 2. Start again.");
+		}
+		
+		System.out.println("*** BMI CATEGORIES ***");
+		System.out.print("Underweight = <18.5\nNormal Weight = 18.5-24.9\nOverweight = 25-29.9\nObesity = 30+");
+		
+
+	}
+
+}
---
+++ blanco_PA2/blanco_p3/src/blanco_03/blanco_03.java
@@ -0,0 +1,10 @@
+package blanco_03;
+
+public class blanco_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
